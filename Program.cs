68d9a179c3acb493c5b8ae0f0bbc11c15223a8e7
Vadim Pudov <pvb@rambler.ru>
2022-11-13 19:24:38 +0700

    added inputs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int size = 7;
+string[] array = new string[size];
+int index = 0;
+string input = "";
+
+Console.Write("\nВведите 7 (семь) элементов массива");
